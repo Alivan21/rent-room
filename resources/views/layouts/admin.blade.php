d4df0f64097e61d0e0f4d12b1cb18cfaa60d8f2d
alfanalivan <alfanalivan@gmail.com>
2023-08-05 16:12:53 +0700

    feat:halaman dashboard

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('header')
+  @include('components.AdminNavbar')
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Pinjam Ruang</title>
+  {{-- Fonts --}}
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+
+  {{-- Icon --}}
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="bg-gray-50 p-5">
+  @yield('header')
+  <main class="bg-white p-5 rounded h-screen sm:ml-64">
+    @yield('content')
+  </main>
+  @yield('footer')
+</body>
+
+</html>
